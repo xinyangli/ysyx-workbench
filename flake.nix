bd8a6c4d6c0a293d2dbaca4138748764143754d2
tracer-ysyx <tracer@ysyx.org>
2023-12-21 00:31:43 +0800

    > compile NEMU ysyx_22040000 李心杨 Linux calcite 6.1.65 #1-NixOS SMP PREEMPT_DYNAMIC Sun Dec  3 06:32:13 UTC 2023 x86_64 GNU/Linux  00:31:43  up 145 days  7:04,  2 users,  load average: 0.30, 0.48, 0.49

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, ... }@inputs: with inputs; 
+    flake-utils.lib.eachDefaultSystem (system:
+     let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            gnumake
+            bison
+            flex
+            ncurses
+            readline
+            llvmPackages_15.libllvm
+          ];
+
+          shellHook = ''
+            export NEMU_HOME=/home/xin/repo/ysyx-workbench/nemu
+            export AM_HOME=/home/xin/repo/ysyx-workbench/abstract-machine
+          '';
+        };
+      }
+    );
+}
