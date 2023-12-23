97df569747670a240dc8b0c05c6aa11cb7bd0c0a
xinyangli <lixinyang411@gmail.com>
2023-12-23 20:23:18 +0800

    create Makefile for example in verilator manual

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,32 @@
-#include <stdio.h>
+#include <cstdlib>
+#include <cassert>
+#include <cstdlib>
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+#include "Vexample.h"
 
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+#define MAX_SIM_TIME 100
+
+int main(int argc, char **argv, char **env) {
+    int sim_time = 0;
+    Verilated::commandArgs(argc, argv);
+    Vexample *top = new Vexample;
+
+    Verilated::traceEverOn(true);
+    VerilatedVcdC *m_trace = new VerilatedVcdC; 
+    top->trace(m_trace, 5);
+    m_trace->open("waveform.vcd");
+    for (sim_time = 0; sim_time < MAX_SIM_TIME; sim_time++) {
+        int a = rand() & 1;
+        int b = rand() & 1;
+        top->a = a;
+        top->b = b;
+        top->eval();
+        printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+        assert(top->f == (a ^ b));
+        m_trace->dump(sim_time);
+    }
+    m_trace->close();
+    delete top;
+    exit(EXIT_SUCCESS);
 }
