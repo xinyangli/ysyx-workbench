66d0ce915b197edab512bb7ad2b06727603ce47a
tracer-ysyx <tracer@ysyx.org>
2024-01-06 00:43:00 +0800

    > configure  ysyx_22040000 李心杨  Linux calcite 6.1.69 #1-NixOS SMP PREEMPT_DYNAMIC Wed Dec 20 16:00:29 UTC 2023 x86_64 GNU/Linux   00:43:00  up 1 day  9:29,  2 users,  load average: 0.45, 0.46, 0.41

---
+++ git_commit.sh
@@ -0,0 +1,40 @@
+#/usr/bin/env bash
+#
+# Environment Variables:
+#   NEMU_HOME
+set -x
+
+STUID=ysyx_22040000
+STUNAME=李心杨
+
+TRACER=tracer-ysyx
+GITFLAGS="-q --author=$TRACER<tracer@ysyx.org> --no-verify --allow-empty"
+
+YSYX_HOME=$NEMU_HOME/..
+WORK_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+WORK_INDEX=$YSYX_HOME/.git/index.${WORK_BRANCH}
+TRACER_BRANCH=$TRACER
+
+LOCK_DIR=$YSYX_HOME/.git/
+
+git_soft_checkout () {
+    git checkout --detach -q && git reset --soft $1 -q -- && git checkout $1 -q -- ;
+}
+
+git_commit () {
+    # create tracer branch if not existent
+    git branch $TRACER_BRANCH -q 2>/dev/null || true
+    # backup git index
+    cp -a .git/index $WORK_INDEX
+    # switch to tracer branch
+    git_soft_checkout "$TRACER_BRANCH"
+    # add files to commit
+    git add . -A --ignore-errors
+    # generate commit msg, commit changes in tracer branch
+    printf "> $1 \n $STUID $STUNAME \n $(uname -a) \n $(uptime)\n" | git commit -F - $GITFLAGS
+    git_soft_checkout "$WORK_BRANCH"
+    mv $WORK_INDEX .git/index
+}
+
+git_commit $1
+
