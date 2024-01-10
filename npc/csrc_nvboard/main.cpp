f808bb6b135bdd8ad1408f74b317588d35840604
tracer-ysyx <tracer@ysyx.org>
2024-01-10 14:59:16 +0800

    > build_npc_VSegHandler_nvboard  ysyx_22040000 李心杨  Linux calcite 6.1.69 #1-NixOS SMP PREEMPT_DYNAMIC Wed Dec 20 16:00:29 UTC 2023 x86_64 GNU/Linux   14:59:16  up 2 days 14:00,  2 users,  load average: 1.66, 1.78, 1.59

--- npc/csrc_nvboard/main.cpp
+++ npc/csrc_nvboard/main.cpp
@@ -8,10 +8,10 @@
 
 const int MAX_SIM_TIME=100;
 
-void nvboard_bind_all_pins(VKeyboard* top);
+void nvboard_bind_all_pins(VSegHandler* top);
 
 int main(int argc, char **argv, char **env) {
-    VKeyboard *top = new VKeyboard;
+    VSegHandler *top = new VSegHandler;
 
     nvboard_bind_all_pins(top);
     nvboard_init();
