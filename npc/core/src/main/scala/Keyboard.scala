3b03aee93770df71c1c71dafdd08beb8b858a158
tracer-ysyx <tracer@ysyx.org>
2024-01-10 17:29:22 +0800

    > configure(npc)  ysyx_22040000 李心杨  Linux calcite 6.1.69 #1-NixOS SMP PREEMPT_DYNAMIC Wed Dec 20 16:00:29 UTC 2023 x86_64 GNU/Linux   17:29:22  up 2 days 16:30,  2 users,  load average: 1.28, 0.94, 0.82

--- npc/core/src/main/scala/Keyboard.scala
+++ npc/core/src/main/scala/Keyboard.scala
@@ -78,17 +78,17 @@ class SegGenerator(seg_count: Int) extends Module {
     "b01100011".U, "b10000101".U, "b01100001".U, "b01110001".U,
   ))
 
-  val keycode_to_ascii = (((0x41 to 0x5A) ++ (0x30 to 0x39)).map(_.U)).zip(Seq(
+  val keycode_to_ascii = Seq(
     0x1C.U, 0x32.U, 0x21.U, 0x23.U, 0x24.U, 0x2B.U,
     0x34.U, 0x33.U, 0x43.U, 0x3B.U, 0x42.U, 0x4B.U,
     0x3A.U, 0x31.U, 0x44.U, 0x4D.U, 0x15.U, 0x2D.U,
     0x1B.U, 0x2C.U, 0x3C.U, 0x2A.U, 0x1D.U, 0x22.U,
     0x35.U, 0x1A.U, 0x45.U, 0x16.U, 0x1E.U, 0x26.U,
     0x25.U, 0x2E.U, 0x36.U, 0x3D.U, 0x3E.U, 0x46.U,
-  ))
+  ).zip(((0x41 to 0x5A) ++ (0x30 to 0x39)).map(_.U))
 
   // val keycode = Mux(io.keycode.ready && io.keycode.valid, io.keycode.bits, keycode)
-  val keycode = RegEnable(io.keycode.bits, 0.U, io.keycode.ready && io.keycode.valid)
+  val keycode = RegEnable(io.keycode.bits, io.keycode.ready && io.keycode.valid)
   val keycode_digits = VecInit(keycode(3,0)) ++ VecInit(keycode(7,4))
   val keycode_seg = keycode_digits.map(MuxLookup(_, 0xFF.U)(digit_to_seg))
   val ascii = MuxLookup(keycode, 0.U)(keycode_to_ascii)
