cc29fd2f1c98134c6f227c7ab02a89f36ab98002
tracer-ysyx <tracer@ysyx.org>
2024-01-11 22:43:45 +0800

    > compile NEMU ysyx_22040000 李心杨 Linux calcite 6.1.69 #1-NixOS SMP PREEMPT_DYNAMIC Wed Dec 20 16:00:29 UTC 2023 x86_64 GNU/Linux  22:43:45  up 3 days 21:44,  2 users,  load average: 0.82, 0.84, 0.90

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -189,7 +189,7 @@ static int cmd_x(char *args) {
   word_t addr = parse_expr(arg, &res);
   if (!res)
     goto wrong_usage;
-  addr = addr & ~0x3;
+  addr = addr & ~(WORD_BYTES - 1);
   for (paddr_t paddr = addr; paddr < addr + n;
        paddr += WORD_BYTES) {
     word_t value = paddr_read(addr, WORD_BYTES);
