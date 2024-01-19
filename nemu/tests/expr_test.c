6ae1b559b827c0cf5de9c6ea9130a56c92e554f5
tracer-ysyx <tracer@ysyx.org>
2024-01-19 20:57:13 +0800

    > compile NEMU ysyx_22040000 李心杨 Linux calcite 6.1.71 #1-NixOS SMP PREEMPT_DYNAMIC Fri Jan  5 14:18:41 UTC 2024 x86_64 GNU/Linux  20:57:13  up   0:12,  2 users,  load average: 1.12, 0.95, 0.54

--- nemu/tests/expr_test.c
+++ nemu/tests/expr_test.c
@@ -173,7 +173,7 @@ START_TEST(test_expr_plain_register) {
   uint32_t value;
   // NOTE: need to fix this if want to support more arch
   buf[0] = '$';
-  for (i = 1; i < 32; i++) {
+  for (i = 0; i < 32; i++) {
     ck_assert(strncpy(buf + 1, regs[i], 10) != NULL);
     gpr(i) = i;
     yy_scan_string(buf);
