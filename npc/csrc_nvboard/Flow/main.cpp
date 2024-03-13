2c591550f7972db139fe40cfc68dada6f932b914
tracer-ysyx <tracer@ysyx.org>
2024-03-13 14:09:24 +0800

    > build_flow_VFlow  ysyx_22040000 李心杨  Linux calcite 6.6.19 #1-NixOS SMP PREEMPT_DYNAMIC Fri Mar  1 12:35:11 UTC 2024 x86_64 GNU/Linux   14:09:24  up   3:45,  2 users,  load average: 1.43, 0.67, 0.85

--- npc/csrc_nvboard/Flow/main.cpp
+++ npc/csrc_nvboard/Flow/main.cpp
@@ -1,11 +1,11 @@
 #include <verilated.h>
 #include <verilated_vcd_c.h>
-#include <nvboard.h>
+// #include <nvboard.h>
 #include <VFlow.h>
 
 const int MAX_SIM_TIME=100;
 
-void nvboard_bind_all_pins(VFLow* top);
+// void nvboard_bind_all_pins(VFLow* top);
 
 int main(int argc, char **argv, char **env) {
     VFlow *top = new VFlow;
