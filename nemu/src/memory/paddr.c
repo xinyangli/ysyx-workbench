ed8cd21c75db5e2d28bf5f1142fd02d61d9e87c0
tracer-ysyx <tracer@ysyx.org>
2024-03-13 17:04:10 +0800

    > compile NEMU ysyx_22040000 李心杨 Linux calcite 6.6.19 #1-NixOS SMP PREEMPT_DYNAMIC Fri Mar  1 12:35:11 UTC 2024 x86_64 GNU/Linux  17:04:10  up   6:40,  2 users,  load average: 0.24, 0.29, 0.46

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -53,7 +53,7 @@ void init_mem() {
 word_t paddr_read(paddr_t addr, int len) {
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
-  IFDEF(CONFIG_MTRACE, printf("R " FMT_WORD "%d\n", addr, len));
+  IFDEF(CONFIG_MTRACE, Log("R " FMT_WORD "%d\n", addr, len));
   out_of_bound(addr);
   return 0;
 }
@@ -61,6 +61,6 @@ word_t paddr_read(paddr_t addr, int len) {
 void paddr_write(paddr_t addr, int len, word_t data) {
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
-  IFDEF(CONFIG_MTRACE, printf("W " FMT_WORD "%d D" FMT_WORD "\n", addr, len, data));
+  IFDEF(CONFIG_MTRACE, Log("W " FMT_WORD "%d D" FMT_WORD "\n", addr, len, data));
   out_of_bound(addr);
 }
