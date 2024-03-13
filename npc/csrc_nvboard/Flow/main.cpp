020659b74e078efb9422d35b916ee184499508d7
tracer-ysyx <tracer@ysyx.org>
2024-03-13 14:09:40 +0800

    > configure(npc)  ysyx_22040000 李心杨  Linux calcite 6.6.19 #1-NixOS SMP PREEMPT_DYNAMIC Fri Mar  1 12:35:11 UTC 2024 x86_64 GNU/Linux   14:09:40  up   3:45,  2 users,  load average: 1.36, 0.71, 0.86

--- npc/csrc_nvboard/Flow/main.cpp
+++ npc/csrc_nvboard/Flow/main.cpp
@@ -8,12 +8,5 @@ const int MAX_SIM_TIME=100;
 // void nvboard_bind_all_pins(VFLow* top);
 
 int main(int argc, char **argv, char **env) {
-    VFlow *top = new VFlow;
-
-    // nvboard_init();
-    while (true) {
-        // nvboard_update();
-        top->eval();
-    }
-    delete top;
+    return 0;
 }
