01001c930b13804ecf551368d3ae74534ef3c62a
tracer-ysyx <tracer@ysyx.org>
2024-03-20 19:16:13 +0800

    > compile NEMU ysyx_22040000 李心杨 Linux calcite 6.6.19 #1-NixOS SMP PREEMPT_DYNAMIC Fri Mar  1 12:35:11 UTC 2024 x86_64 GNU/Linux  19:16:13  up   6:22,  2 users,  load average: 0.18, 0.37, 0.44

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -107,9 +107,10 @@ void ftrace_call(vaddr_t pc, vaddr_t addr) {
 }
 
 void ftrace_return(vaddr_t pc, vaddr_t addr) {
-  for (; addr != ftrace_stack[ftrace_stack_pos] && ftrace_stack_pos >= 0;
-       ftrace_stack_pos--) {
-    Trace("%*s0x%x ret <TCO> [%s@0x%x] ", ftrace_stack_pos, "", pc, get_func_name(addr), addr);
-  }
+
+  // for (; addr != ftrace_stack[ftrace_stack_pos] && ftrace_stack_pos >= 0;
+  //      ftrace_stack_pos--) {
+  //   Trace("%*s0x%x ret <TCO> [%s@0x%x] ", ftrace_stack_pos, "", pc, get_func_name(addr), addr);
+  // }
   Trace("%*s0x%x ret [%s@0x%x]", ftrace_stack_pos, "", pc, get_func_name(addr), addr);
 }
