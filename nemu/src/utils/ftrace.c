d9fa92afb9817bfe79c96b63bfa2461f362ba893
tracer-ysyx <tracer@ysyx.org>
2024-03-20 18:49:18 +0800

    > compile NEMU ysyx_22040000 李心杨 Linux calcite 6.6.19 #1-NixOS SMP PREEMPT_DYNAMIC Fri Mar  1 12:35:11 UTC 2024 x86_64 GNU/Linux  18:49:18  up   5:55,  2 users,  load average: 1.04, 0.66, 0.53

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -1,3 +1,4 @@
+#include "debug.h"
 #include "macro.h"
 #include <assert.h>
 #include <common.h>
@@ -100,10 +101,10 @@ failed_nosym:
 }
 
 void ftrace_call(vaddr_t pc, vaddr_t addr) {
-  printf("[FTRACE] 0x%x call [%s@0x%x]\n", pc, get_func_name(addr), addr);
+  Trace("0x%x call [%s@0x%x]\n", pc, get_func_name(addr), addr);
 }
 
 void ftrace_return(vaddr_t pc, vaddr_t addr) {
-  printf("[FTRACE] 0x%x ret [%s@0x%x]\n", pc, get_func_name(addr), addr);
+  Trace("0x%x ret [%s@0x%x]\n", pc, get_func_name(addr), addr);
 }
 
