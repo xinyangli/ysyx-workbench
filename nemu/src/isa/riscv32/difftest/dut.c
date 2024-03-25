f9bd7e25b73a9643a3a81155f4dd9bfb71ccfb35
tracer-ysyx <tracer@ysyx.org>
2024-03-26 01:49:53 +0800

    > compile NEMU ysyx_22040000 李心杨 Linux calcite 6.6.19 #1-NixOS SMP PREEMPT_DYNAMIC Fri Mar  1 12:35:11 UTC 2024 x86_64 GNU/Linux  01:49:53  up  11:25,  2 users,  load average: 0.82, 0.52, 0.43

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -19,9 +19,9 @@
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for(int i = 0; i < MUXDEF(CONFIG_RVE, 16, 32); i++) {
-    difftest_check_reg(reg_name(i), pc, ref_r->gpr[i], gpr(i));
+    if(!difftest_check_reg(reg_name(i), pc, ref_r->gpr[i], gpr(i))) return false;
   }
-  return false;
+  return true;
 }
 
 void isa_difftest_attach() {
