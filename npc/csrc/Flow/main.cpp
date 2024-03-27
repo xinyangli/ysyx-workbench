bbd1761b64996ebefbd0faa7cb6d9b6ab5dab562
tracer-ysyx <tracer@ysyx.org>
2024-03-27 17:25:34 +0800

    > build_flow_VFlow  ysyx_22040000 李心杨  Linux calcite 6.6.22 #1-NixOS SMP PREEMPT_DYNAMIC Fri Mar 15 18:25:07 UTC 2024 x86_64 GNU/Linux   17:25:34  up   0:34,  2 users,  load average: 1.53, 1.30, 1.19

--- npc/csrc/Flow/main.cpp
+++ npc/csrc/Flow/main.cpp
@@ -1,43 +1,56 @@
-#include <cstdlib>
+#include "vpi_user.h"
+#include <VFlow.h>
 #include <cstdlib>
 #include <verilated.h>
 #include <verilated_vcd_c.h>
 #include <verilated_vpi.h>
-#include <VFlow.h>
 #define MAX_SIM_TIME 100
 #define VERILATOR_TRACE
 
+int vpiGetInt(const char *name) {
+  vpiHandle vh1 = vpi_handle_by_name((PLI_BYTE8 *)name, NULL);
+  if(!vh1)
+    vl_fatal(__FILE__, __LINE__, "sim_main", "No handle found");
+  s_vpi_value v;
+  v.format = vpiIntVal;
+  vpi_get_value(vh1, &v);
+  return v.value.integer;
+}
+  
+
 int main(int argc, char **argv, char **env) {
-    int sim_time = 0;
-    Verilated::commandArgs(argc, argv);
+  int sim_time = 0;
+  Verilated::commandArgs(argc, argv);
 
-    VFlow *top = new VFlow;
+  VFlow *top = new VFlow;
 
-    Verilated::traceEverOn(true);
-    VerilatedVcdC *m_trace = new VerilatedVcdC; 
+  Verilated::traceEverOn(true);
+  VerilatedVcdC *m_trace = new VerilatedVcdC;
 #ifdef VERILATOR_TRACE
-    top->trace(m_trace, 5);
-    m_trace->open("waveform.vcd");
+  top->trace(m_trace, 5);
+  m_trace->open("waveform.vcd");
 #endif
-    for (sim_time = 0; sim_time < 10; sim_time++) {
-        top->eval();
-        top->clock = !top->clock;
-        top->reset = 1;
+  for (sim_time = 0; sim_time < 10; sim_time++) {
+    top->eval();
+    top->clock = !top->clock;
+    top->reset = 1;
 #ifdef VERILATOR_TRACE
-        m_trace->dump(sim_time);
+    m_trace->dump(sim_time);
 #endif
-    }
-    top->reset = 0;
-    for (sim_time = 10; sim_time < MAX_SIM_TIME; sim_time++) {
-        top->eval();
-        top->clock = !top->clock;
+  }
+  top->reset = 0;
+  for (sim_time = 10; sim_time < MAX_SIM_TIME; sim_time++) {
+    top->eval();
+    top->clock = !top->clock;
+    int o = vpiGetInt("TOP.Flow.reg_0.regFile_3");
+    printf("%d\n", o);
 #ifdef VERILATOR_TRACE
-        m_trace->dump(sim_time);
+    m_trace->dump(sim_time);
 #endif
-    }
+  }
 #ifdef VERILATOR_TRACE
-    m_trace->close();
+  m_trace->close();
 #endif
-    delete top;
-    exit(EXIT_SUCCESS);
+  delete top;
+  exit(EXIT_SUCCESS);
 }
