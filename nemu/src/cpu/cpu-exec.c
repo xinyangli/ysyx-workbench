5691d5b74581e1100c5cde4117a2ad87d566cb43
tracer-ysyx <tracer@ysyx.org>
2024-04-04 20:52:47 +0800

    > compile NEMU ysyx_22040000 李心杨 Linux calcite 6.6.22 #1-NixOS SMP PREEMPT_DYNAMIC Fri Mar 15 18:25:07 UTC 2024 x86_64 GNU/Linux  20:52:47  up 3 days  0:57,  2 users,  load average: 0.96, 0.97, 1.05

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -79,6 +79,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
 void execute(uint64_t n) {
   Decode s;
   for (;n > 0; n --) {
+    printf("n: %lu\n", n);
     exec_once(&s, cpu.pc);
     g_nr_guest_inst ++;
     trace_and_difftest(&s, cpu.pc);
