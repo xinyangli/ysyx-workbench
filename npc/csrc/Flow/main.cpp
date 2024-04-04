c13f5cf409f41d9c42ab6359772e622a3422c6df
tracer-ysyx <tracer@ysyx.org>
2024-04-04 19:54:08 +0800

    > compile NEMU ysyx_22040000 李心杨 Linux calcite 6.6.22 #1-NixOS SMP PREEMPT_DYNAMIC Fri Mar 15 18:25:07 UTC 2024 x86_64 GNU/Linux  19:54:08  up 2 days 23:58,  2 users,  load average: 0.40, 0.49, 0.78

--- npc/csrc/Flow/main.cpp
+++ npc/csrc/Flow/main.cpp
@@ -1,30 +1,29 @@
-#include "components.hpp"
 #include "config.hpp"
 #include "vl_wrapper.hpp"
+#include "components.hpp"
 #include <VFlow.h>
 
 using VlModule = VlModuleInterfaceCommon<VFlow>;
 using Registers = _RegistersVPI<uint32_t, 32>;
 
 extern "C" {
-void *pmem_get() {
-  static auto pmem = new Memory<int, 128 * 1024>(config.memory_file,
-                                                 config.memory_file_binary);
-  return pmem;
-}
+  void *pmem_get() {
+    static auto pmem = new Memory<int, 128 * 1024>(config.memory_file, config.memory_file_binary);
+    return pmem;
+  }
 
-int pmem_read(int raddr) {
-  void *pmem = pmem_get();
-  auto mem = static_cast<Memory<int, 128 * 1024> *>(pmem);
-  return mem->read(raddr);
-}
+  int pmem_read(int raddr) {
+    void *pmem = pmem_get();
+    auto mem = static_cast<Memory<int, 128 * 1024>*>(pmem);
+    return mem->read(raddr);
+  }
 
-void pmem_write(int waddr, int wdata, char wmask) {
-  void *pmem = pmem_get();
-  auto mem = static_cast<Memory<int, 128 * 1024> *>(pmem);
-  return mem->write((std::size_t)waddr, wdata, wmask);
-}
-}
+  void pmem_write(int waddr, int wdata, char wmask) {
+    void *pmem = pmem_get();
+    auto mem = static_cast<Memory<int, 128 * 1024>*>(pmem);
+    return mem->write((std::size_t)waddr, wdata, wmask);
+  }
+} 
 
 int main(int argc, char **argv, char **env) {
   config.cli_parse(argc, argv);
@@ -33,7 +32,7 @@ int main(int argc, char **argv, char **env) {
 
   top->reset_eval(10);
   for (int i = 0; i < config.max_sim_time; i++) {
-    if (top->is_posedge()) {
+    if(top->is_posedge()) {
       // Posedge
       regs.update();
       regs.print_regs();
