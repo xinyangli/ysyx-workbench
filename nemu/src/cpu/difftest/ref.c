42928a0a2c09c85c994120bcdcc3d266e3e582e5
tracer-ysyx <tracer@ysyx.org>
2024-04-04 20:38:04 +0800

    > compile NEMU ysyx_22040000 李心杨 Linux calcite 6.6.22 #1-NixOS SMP PREEMPT_DYNAMIC Fri Mar 15 18:25:07 UTC 2024 x86_64 GNU/Linux  20:38:04  up 3 days  0:42,  2 users,  load average: 1.15, 1.11, 1.11

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -26,10 +26,10 @@ __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction)
 
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
   // assert(0);
-  // if(direction == DIFFTEST_TO_DUT)
-  //   memcpy(dut, &cpu, sizeof(CPU_state));
-  // else
-  //   memcpy(&cpu, dut, sizeof(CPU_state));
+  if(direction == DIFFTEST_TO_DUT)
+    memcpy(dut, &cpu, sizeof(CPU_state));
+  else
+    memcpy(&cpu, dut, sizeof(CPU_state));
 }
 
 __EXPORT void difftest_exec(uint64_t n) {
