056c8dc38a80d0182b87f4ac836a83286059c465
tracer-ysyx <tracer@ysyx.org>
2024-04-10 01:49:52 +0800

    > compile NEMU ysyx_22040000 李心杨 Linux calcite 6.6.25 #1-NixOS SMP PREEMPT_DYNAMIC Thu Apr  4 18:23:07 UTC 2024 x86_64 GNU/Linux  01:49:52  up  13:36,  2 users,  load average: 0.54, 0.80, 0.91

--- nemu/include/cpu/ifetch.h
+++ nemu/include/cpu/ifetch.h
@@ -20,7 +20,7 @@
 static inline uint32_t inst_fetch(vaddr_t *pc, int len) {
   uint32_t inst = vaddr_ifetch(*pc, len);
   (*pc) += len;
-  printf("Inst fetch: %p %d %x\n", pc, len, inst);
+  printf("Inst fetch: %x %d %x\n", *pc, len, inst);
   return inst;
 }
 
