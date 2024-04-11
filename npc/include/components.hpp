994e94521a2fbb9061a9f597a0685f343773e264
tracer-ysyx <tracer@ysyx.org>
2024-04-11 15:40:03 +0800

    > build_flow_VFlow  ysyx_22040000 李心杨  Linux calcite 6.6.25 #1-NixOS SMP PREEMPT_DYNAMIC Thu Apr  4 18:23:07 UTC 2024 x86_64 GNU/Linux   15:40:03  up 2 days  3:26,  2 users,  load average: 1.27, 1.69, 1.67

--- npc/include/components.hpp
+++ npc/include/components.hpp
@@ -30,7 +30,7 @@ public:
 template <typename T, std::size_t n> class Memory {
   std::size_t addr_to_index(std::size_t addr) {
     if (addr < 0x80000000 || addr > 0x8fffffff) {
-      return 0;
+      throw std::runtime_error("Invalid memory access");
     }
     // Linear mapping
     return (addr >> 2) - 0x20000000;
