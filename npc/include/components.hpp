979da87d66b6a49826710c52a5497c93e8573c43
tracer-ysyx <tracer@ysyx.org>
2024-04-11 16:31:03 +0800

    > build_flow_VFlow  ysyx_22040000 李心杨  Linux calcite 6.6.25 #1-NixOS SMP PREEMPT_DYNAMIC Thu Apr  4 18:23:07 UTC 2024 x86_64 GNU/Linux   16:31:03  up   0:29,  2 users,  load average: 0.76, 0.86, 0.82

--- npc/include/components.hpp
+++ npc/include/components.hpp
@@ -32,7 +32,7 @@ template <typename T, std::size_t n> class Memory {
     static bool is_initial = true;
     if (is_initial) {
       is_initial = false;
-      return (addr >> 2) - 0x20000000;
+      return 0;
     }
     if (addr < 0x80000000 || addr > 0x8fffffff) {
       std::cerr << "ACCESS " << addr << std::endl;
