241d827cf235fc5598c39aa5c49d4cc08d58d43d
tracer-ysyx <tracer@ysyx.org>
2024-04-11 23:26:03 +0800

    > compile NEMU ysyx_22040000 李心杨 Linux calcite 6.6.25 #1-NixOS SMP PREEMPT_DYNAMIC Thu Apr  4 18:23:07 UTC 2024 x86_64 GNU/Linux  23:26:03  up   7:24,  2 users,  load average: 0.92, 0.84, 1.03

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -52,7 +52,7 @@ static void out_of_bound(paddr_t addr) {
 static void mtrace_print(char type, word_t addr, int len, word_t data) {
   for (int i = 0; i < range_count; i++)
     if (addr <= mtrace_end[i] && addr >= mtrace_start[i] ) {
-      Trace("Mem %c " FMT_PADDR "%d D " FMT_PADDR, type, addr, len, data);
+      Trace("Mem %c at "FMT_WORD" " FMT_PADDR "%d D " FMT_PADDR, type, cpu.pc, addr, len, data);
       break;
     }
 }
