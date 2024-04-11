c939768aa967ae8506192c14cea6535404536c4f
tracer-ysyx <tracer@ysyx.org>
2024-04-11 16:35:57 +0800

    > build_flow_VFlow  ysyx_22040000 李心杨  Linux calcite 6.6.25 #1-NixOS SMP PREEMPT_DYNAMIC Thu Apr  4 18:23:07 UTC 2024 x86_64 GNU/Linux   16:35:57  up   0:34,  2 users,  load average: 1.29, 1.03, 0.89

--- npc/include/components.hpp
+++ npc/include/components.hpp
@@ -30,7 +30,7 @@ public:
 
 template <typename T, std::size_t n> class Memory {
   std::size_t addr_to_index(std::size_t addr) {
-    static int skip_initial = 4;
+    static int skip_initial = 8;
     if (addr < 0x80000000 || addr > 0x8fffffff) {
       if (skip_initial) {
         skip_initial--;
