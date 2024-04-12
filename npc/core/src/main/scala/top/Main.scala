ea07b129d9900943b85617d574b53544ee198128
tracer-ysyx <tracer@ysyx.org>
2024-04-12 15:27:07 +0800

    > configure(npc)  ysyx_22040000 李心杨  Linux calcite 6.6.25 #1-NixOS SMP PREEMPT_DYNAMIC Thu Apr  4 18:23:07 UTC 2024 x86_64 GNU/Linux   15:27:07  up  23:25,  2 users,  load average: 1.44, 1.26, 1.17

--- npc/core/src/main/scala/top/Main.scala
+++ npc/core/src/main/scala/top/Main.scala
@@ -32,6 +32,7 @@ object VerilogMain extends App {
     }
     case None => Config(traceConfig = TraceConfig(enable = true))
   }
+
   val annos = (new ChiselStage).execute(
     Array("--target-dir", opt.targetDir.toString, "--target", "systemverilog", "--split-verilog", "--full-stacktrace"),
     Seq(
