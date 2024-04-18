37f152cd411629af67cd2359367115a38c882282
tracer-ysyx <tracer@ysyx.org>
2024-04-18 20:30:41 +0800

    > build_flow_VFlow  ysyx_22040000 李心杨  Linux calcite 6.6.25 #1-NixOS SMP PREEMPT_DYNAMIC Thu Apr  4 18:23:07 UTC 2024 x86_64 GNU/Linux   20:30:41  up 1 day 21:24,  2 users,  load average: 1.42, 1.62, 1.65

--- npc/csrc/devices/include/devices.hpp
+++ npc/csrc/devices/include/devices.hpp
@@ -25,6 +25,7 @@ class Serial : public Device {
   std::array<uint8_t, 1> buf;
   public:
     Serial(uint64_t addr, size_t len);
+    ~Serial();
     void io_handler(uint32_t offset, size_t len, bool is_write) override;
     void transfer(uint8_t *src, size_t len, bool is_write) override;
 };
