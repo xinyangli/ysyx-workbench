b6d7f7370d215a8b9910f560b9326a16d321c152
tracer-ysyx <tracer@ysyx.org>
2024-07-09 16:13:23 +0800

    > compile NEMU ysyx_22040000 李心杨 Linux calcite 6.6.32 #1-NixOS SMP PREEMPT_DYNAMIC Sat May 25 14:22:56 UTC 2024 x86_64 GNU/Linux  16:13:23  up   5:49,  2 users,  load average: 1.09, 0.67, 0.50

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -76,6 +76,7 @@ static long load_img() {
 
     fp = fopen(file_path, "rb");
     Assert(fp != NULL || errno == ENOENT, "Cannot open '%s'", img_file);
+    if(fp) Log("Found '%s' in '%s'", img_file, path_start);
     path_start = path_end + 1;
   } while(path_end != NULL);
   free(search_paths);
