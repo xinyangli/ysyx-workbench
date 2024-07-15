2a259d2f79597aab6c67c489449964ae07db6441
tracer-ysyx <tracer@ysyx.org>
2024-07-15 15:45:33 +0800

    > compile NEMU ysyx_22040000 李心杨 Linux calcite 6.6.37 #1-NixOS SMP PREEMPT_DYNAMIC Fri Jul  5 07:34:07 UTC 2024 x86_64 GNU/Linux  15:45:33  up   5:58,  2 users,  load average: 1.17, 1.06, 0.86

--- nemu/src/monitor/gdbstub.cc
+++ nemu/src/monitor/gdbstub.cc
@@ -71,14 +71,9 @@ __EXPORT void nemu_cont(void *args, gdb_action_t *res) {
 
 __EXPORT void nemu_stepi(void *args, gdb_action_t *res) {
   DbgState *dbg_state = (DbgState *)args;
-  printf("%p, args: %p, halt: %d, p_res: %p\n", dbg_state->bp, args, dbg_state->halt, res);
   breakpoint_t *stopped_at =
       cpu_exec_with_bp(1, dbg_state->bp->data(), dbg_state->bp->size());
-  printf("exec done.\n");
-  printf("res: %p, stopped at (r): %p.\n", res, stopped_at);
   nemu_is_stopped(res, stopped_at);
-  printf("is_stopped done.\n");
-  fflush(stdout);
 }
 
 __EXPORT bool nemu_set_bp(void *args, size_t addr, bp_type_t type) {
