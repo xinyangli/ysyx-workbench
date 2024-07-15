e59c4d9267cfa7beda1fc41bd8dc1ef82fa17472
tracer-ysyx <tracer@ysyx.org>
2024-07-15 15:13:38 +0800

    > compile NEMU ysyx_22040000 李心杨 Linux calcite 6.6.37 #1-NixOS SMP PREEMPT_DYNAMIC Fri Jul  5 07:34:07 UTC 2024 x86_64 GNU/Linux  15:13:38  up   5:26,  2 users,  load average: 1.35, 0.76, 0.77

--- nemu/src/monitor/gdbstub.cc
+++ nemu/src/monitor/gdbstub.cc
@@ -122,7 +122,7 @@ static gdbstub_t gdbstub_priv;
 __EXPORT void nemu_init(void *args) {
   DbgState *dbg_state = (DbgState *)args;
   dbg_state->bp = new std::vector<breakpoint_t>();
-  printf("bp: %p, bp alloc: %p\n", dbg_state, dbg_state->bp);
+  printf("bp: %p, bp alloc: %p, bp data: %p\n", dbg_state, dbg_state->bp, dbg_state->bp->data());
   Assert(dbg_state->bp != NULL, "Failed to allocate breakpoint");
 
   void init_mem();
