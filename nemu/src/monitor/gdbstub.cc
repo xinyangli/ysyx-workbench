05fc54269ddaebaf255ddc2f1b9171bd09e33c77
tracer-ysyx <tracer@ysyx.org>
2024-07-16 17:34:36 +0800

    > compile NEMU ysyx_22040000 李心杨 Linux calcite 6.6.37 #1-NixOS SMP PREEMPT_DYNAMIC Fri Jul  5 07:34:07 UTC 2024 x86_64 GNU/Linux  17:34:36  up 1 day  7:47,  2 users,  load average: 1.05, 0.88, 0.85

--- nemu/src/monitor/gdbstub.cc
+++ nemu/src/monitor/gdbstub.cc
@@ -36,7 +36,7 @@ static void nemu_is_stopped(gdb_action_t *act, breakpoint_t *stopped_at) {
   case NEMU_RUNNING:
     nemu_state.state = NEMU_STOP;
     if (stopped_at == NULL) {
-      act->reason = gdb_action_t::ACT_NONE;
+      act->reason = gdb_action_t::ACT_RESUME;
     } else {
       switch (stopped_at->type) {
       case BP_SOFTWARE:
@@ -57,7 +57,6 @@ static void nemu_is_stopped(gdb_action_t *act, breakpoint_t *stopped_at) {
     break;
 
   default:
-    puts("hit shutdown");
     act->reason = gdb_action_t::ACT_SHUTDOWN;
     act->data = nemu_state.halt_ret;
   }
