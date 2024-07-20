421b3a5cdb131f006c840bcec317f7ce838fad42
tracer-ysyx <tracer@ysyx.org>
2024-07-20 16:58:52 +0800

    > configure(npc)  ysyx_22040000 李心杨  Linux calcite 6.6.37 #1-NixOS SMP PREEMPT_DYNAMIC Fri Jul  5 07:34:07 UTC 2024 x86_64 GNU/Linux   16:58:52  up   7:01,  2 users,  load average: 1.43, 1.34, 1.53

--- npc/csrc/Flow/main.cpp
+++ npc/csrc/Flow/main.cpp
@@ -97,6 +97,9 @@ int npc_read_reg(void *args, int regno, size_t *value) {
 int npc_write_reg(void *args, int regno, size_t value) { return 1; }
 
 inline void breakpoint_to_action(const Breakpoint *bp, gdb_action_t *res) {
+  if (bp == nullptr) {
+    res->reason = gdb_action_t::ACT_NONE;
+  }
   switch (bp->type) {
   case BP_SOFTWARE:
     res->reason = gdb_action_t::ACT_BREAKPOINT;
@@ -186,7 +189,7 @@ int gdbstub_loop() {
                                 .on_interrupt = NULL};
 
   if (!gdbstub_init(&gdbstub_priv, &npc_gdbstub_ops, (arch_info_t)isa_arch_info,
-                    strdup("/tmp/gdbstub-npc.sock"))) {
+                    "/tmp/gdbstub-npc.sock")) {
     return EINVAL;
   }
   npc_init(&dbg);
