c046346b386b837b53afb3200dd98cc5e318b2fd
tracer-ysyx <tracer@ysyx.org>
2024-07-22 15:35:16 +0800

    > compile NEMU ysyx_22040000 李心杨 Linux calcite 6.6.40 #1-NixOS SMP PREEMPT_DYNAMIC Mon Jul 15 07:24:53 UTC 2024 x86_64 GNU/Linux  15:35:16  up   4:21,  2 users,  load average: 0.93, 1.03, 0.77

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -21,7 +21,7 @@ void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
 
-bool nemu_do_difftest = true;
+__EXPORT bool nemu_do_difftest = true;
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
