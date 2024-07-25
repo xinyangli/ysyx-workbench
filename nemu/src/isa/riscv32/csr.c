9ff44e86ab0e44555d7b9e03b314be7c863c369b
tracer-ysyx <tracer@ysyx.org>
2024-07-25 12:07:08 +0800

    > compile NEMU ysyx_22040000 李心杨 Linux calcite 6.6.40 #1-NixOS SMP PREEMPT_DYNAMIC Mon Jul 15 07:24:53 UTC 2024 x86_64 GNU/Linux  12:07:08  up   1:59,  2 users,  load average: 1.22, 0.87, 0.86

--- nemu/src/isa/riscv32/csr.c
+++ nemu/src/isa/riscv32/csr.c
@@ -3,6 +3,7 @@
 void init_csr(csr_t csr) { memset(csr, 0, sizeof(word_t)); }
 
 void write_csr(csr_t csr, csr_addr_t csrnum, word_t value) {
+  printf("write csr: %hi %d\n", csrnum, value);
   switch (csrnum) {
   default:
     csr[csrnum] = value;
