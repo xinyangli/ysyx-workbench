3d1c06cb98899efc8e7c12bb486e5c71d399022b
tracer-ysyx <tracer@ysyx.org>
2024-07-26 13:33:52 +0800

    > compile NEMU ysyx_22040000 李心杨 Linux calcite 6.6.40 #1-NixOS SMP PREEMPT_DYNAMIC Mon Jul 15 07:24:53 UTC 2024 x86_64 GNU/Linux  13:33:52  up 1 day  3:26,  2 users,  load average: 1.45, 1.45, 1.30

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,7 +4,7 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  uint32_t keycode = inl(KBD_ADDR) & (~KEYDOWN_MASK);
+  uint32_t keycode = inl(KBD_ADDR);
   kbd->keydown = ((keycode & KEYDOWN_MASK) != 0);
-  kbd->keycode = keycode;
+  kbd->keycode = keycode & (~KEYDOWN_MASK);
 }
